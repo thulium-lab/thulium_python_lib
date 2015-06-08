d874a9dae80eb9af10991f4e34877e6bdfa2a633
thulium-lab <thuliumlab.lpi@gmail.com>
2015-06-08 20:54:55 +0300

    Gaussian functions
    Изменил гауссовы функции, чтобы они совпадали с теми, что в математике
    (для правильного расчета температуры)

--- image_processing.py
+++ image_processing.py
@@ -11,18 +11,19 @@ from numpy import *
 
 # In[ ]:
 
+# removed coeffitioent 2 in gaussian functions in exponents
 def gaussian(x,N,x0,sigma, background):
     """Returns value of a 1D-gaussian with the given parameters"""
     #from numpy import sqrt,pi,exp
-    return N / (sigma * sqrt(2 * pi)) * exp(-(x - x0)**2/(2 * sigma**2)) + background
+    return N / (sigma * sqrt(pi)) * exp(-(x - x0)**2/(sigma**2)) + background
 
 def gaussian2D(N, x0, y0, sigma_x, sigma_y, background):
     """Returns a 2D-gaussian function with the given parameters"""
     #from numpy import pi,exp
     sigma_x = float(sigma_x)
     sigma_y = float(sigma_y)
-    return lambda x,y: N / (sigma_x * sigma_y * 2 * pi) * exp(
-                        -(((x - x0) / sigma_x)**2 + ((y - y0) / sigma_y)**2) / 2) + background
+    return lambda x,y: N / (sigma_x * sigma_y  * pi) * exp(
+                        -(((x - x0) / sigma_x)**2 + ((y - y0) / sigma_y)**2)) + background
 
 
 # In[ ]:
