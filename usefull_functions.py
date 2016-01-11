0cd04dda91c5517776a015c3716d88cbce635800
Artem Golovizin <vizin-90@mail.ru>
2016-01-11 09:29:38 +0300

    after holidays

--- usefull_functions.py
+++ usefull_functions.py
@@ -3,24 +3,35 @@
 
 # In[ ]:
 
-from numpy import *
+"""
+Module for usefull functions
+"""
+
+
+# In[ ]:
+
+import numpy as np
 
 def exp_decay(t, N0, tau, background):
-    return N0 * exp(- t / tau) + background
+    r'N0 * np.exp(- t / tau) + background'
+    return N0 * np.exp(- t / tau) + background
 
 def exp_decay_no_bg(t, N0, tau):
-    return N0 * exp(- t / tau)
+    r'N0 * np.exp(- t / tau)'
+    return N0 * np.exp(- t / tau)
 
 def cloud_expansion(t, T, r0, t0):
+    r'np.sqrt(r0**2 + 2 * k_b * T * (t + 1*t0)**2 / m)'
     k_b = 1.38e-23
     m = 169 * 1.66e-27
-    return sqrt(r0**2 + 2 * k_b * T * (t + 1*t0)**2 / m)
+    return np.sqrt(r0**2 + 2 * k_b * T * (t + 1*t0)**2 / m)
 
 def cloud_expansion0(t, T, r0):
+    r'cloud_expansion(t, T, r0, 0)'
     return cloud_expansion(t, T, r0, 0)
 
 def exp_grouth(t, N0, tau, background):
-    return N0 * ( 1 - exp( - t / tau)) + 0*background
+    return N0 * ( 1 - np.exp( - t / tau)) + 0*background
 
 def construct_fit_description(fit_func, popt_T):
     """constructs a set of string of type 'variable=value\n' for all [1:] function variables"""
@@ -31,4 +42,22 @@ def construct_fit_description(fit_func, popt_T):
         res += str(item[0]) + ' =   ' + '\t'.join(['%.2f'%(x) for x in params]) + '\n'
     res = res.rstrip('\n')
     return res
+
+
+# ### Including some losses 
+
+# In[ ]:
+
+def tow_body_loss(t, N0, betta, background):
+    r'return 1 / ( betta * t + 1 / N0) + background'
+    return 1 / ( betta * t + 1 / N0) + background
+def exp_plus_tw_body_decay(t, N0, tau, betta,  background):
+    r'return N0 * exp(- t / tau) / ( 1 + betta * N0 * tau * (1 - exp( -t / tau))) + 0 * background'
+    return N0 * exp(- t / tau) / ( 1 + betta * N0 * tau * (1 - exp( -t / tau))) + 0 * background
+def two_frac_decay(t, N0, N1, tau, betta,  background):
+    r'return exp_decay(t, N0, tau, 0) + exp_plus_tw_body_decay(t,N1, tau, betta,  0) + abs(background)'
+    return exp_decay(t, N0, tau, 0) + exp_plus_tw_body_decay(t,N1, tau, betta,  0) + abs(background)
+def two_frac_decay_no_bg(t, N0, N1, tau, betta, background):
+    r'return two_frac_decay(t, N0, N1, tau, betta,0)'
+    return two_frac_decay(t, N0, N1, tau, betta,0)
 
